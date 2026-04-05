529bd5eb3fe3a45e4ab35a79468baf93de802efc
MaiHuynhDuongTuanKiet-22664321 <maihuynhduongtuankiet7924@gmail.com>
2026-04-05 16:46:54 +0700

    update

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,14 +1,16 @@
 const User = require("../models/user.model");
 const config = require("../config/auth.config");
 const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
 exports.register = async (req, res) => {
     try {
         const { username, password, full_name } = req.body;
         if (!username || !password || !full_name) return res.status(400).send({ message: "Thiếu dữ liệu!" });
 
-        const savedUser = await User.create({ username, password, full_name, role: 'USER' });
-        res.status(201).send(savedUser);
+        const hashedPassword = bcrypt.hashSync(password, 8);
+        const savedUser = await User.create({ username, password: hashedPassword, full_name, role: 'USER' });
+        res.status(201).send({ id: savedUser.id, username: savedUser.username, full_name: savedUser.full_name, role: savedUser.role });
     } catch (err) {
         res.status(500).send({ message: "Lỗi MariaDB: " + err.message });
     }
@@ -19,7 +21,10 @@ exports.login = async (req, res) => {
         const { username, password } = req.body;
         const user = await User.findByUsername(username);
 
-        if (!user || user.password !== password) return res.status(401).send({ message: "Sai tài khoản/mật khẩu!" });
+        if (!user) return res.status(401).send({ message: "Sai tài khoản/mật khẩu!" });
+        
+        const passwordIsValid = bcrypt.compareSync(password, user.password);
+        if (!passwordIsValid) return res.status(401).send({ message: "Sai tài khoản/mật khẩu!" });
 
         const token = jwt.sign({ id: user.id, role: user.role }, config.secret, { expiresIn: config.jwtExpiration });
         res.status(200).send({ id: user.id, username: user.username, role: user.role, accessToken: token });
